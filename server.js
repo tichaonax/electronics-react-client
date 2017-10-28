502c57e70530d1edf7e1c58d736b5115b92e16c7
Tichaona Hwandaza <tichaona@yahoo.com>
2017-10-28 07:20:24 -0600

    changed port tp 4000 and removed process exit code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-const SERVER_PORT = process.env.PORT || 5000;
+const SERVER_PORT = process.env.PORT || 4000;
 
 app.use((request, response, next) => {
 
@@ -17,11 +17,3 @@ app.use((request, response, next) => {
 app.use(express.static('public'));
 
 app.listen(SERVER_PORT, () => console.log('Express server is up on port ' + SERVER_PORT));
-
-process.on("SIGUSR1", function () {
-    // stop stuff
-    //server.end();
-    // ...
-    // end process
-    process.exit(0);
-});
